3f3984cce17bfa5747766bdf6ee092eaee55c61f
Yann <yann.pavie@outlook.fr>
2024-05-16 16:52:29 +0200

    Add vercel configuration

--- lor_backend/wsgi.py
+++ lor_backend/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lor_backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lor_backend.settings")
 
 application = get_wsgi_application()
+
+app = application
