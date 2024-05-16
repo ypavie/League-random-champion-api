000cf32493974b524e2652092e25e6254f8e62a3
Yann <yann.pavie@outlook.fr>
2024-05-16 17:46:20 +0200

    Update to vercel deployment

--- lor_backend/urls.py
+++ lor_backend/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("random_champion", views.get_random_champion),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
